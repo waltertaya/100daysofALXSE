bf8b294ac749df37c24aee801ed174146e0cec7e
waltertaya <waltermittybecker5@gmail.com>
2024-07-13 21:54:55 +0300

    feat: Add JSON to dictionary conversion function and update main script

--- ArgumentsPython/KeywordArgument/main.py
+++ ArgumentsPython/KeywordArgument/main.py
@@ -5,11 +5,21 @@ Unpacking list to form positional arguments
 import json
 
 
-def dict_to_json(my_dict, filename, **Kwargs):
-    print(f'Kwargs: {Kwargs}')
-    json_txt = json.dumps(my_dict, **Kwargs)
+def dict_to_json(my_dict, filename, **kwargs):
+    print(f'Kwargs: {kwargs}')
+    json_txt = json.dumps(my_dict, **kwargs)
     with open(filename, 'w') as f:
         f.write(json_txt)
+
+def json_to_dict(filename):
+    with open (filename, 'r') as f:
+        my_dict = json.load(f)
+    return my_dict
+
+def function_with_kwargs(**kwargs):
+    print(f'Kwargs: {kwargs}')
+    for key, value in kwargs.items():
+        print(f'{key}: {value}')
 
 
 if __name__ == '__main__':
@@ -17,3 +27,7 @@ if __name__ == '__main__':
     options = {'indent': 4, 'sort_keys': True}
     dict_to_json(my_dict, 'my_dict.json', **options)
     print('File my_dict.json created successfully')
+    my_dict = json_to_dict('my_dict.json')
+    print(my_dict)
+
+    function_with_kwargs(name='John', age=25)
